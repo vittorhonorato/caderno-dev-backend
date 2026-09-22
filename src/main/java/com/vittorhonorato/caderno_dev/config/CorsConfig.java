124f14c03bec428e1be8dea8804b1b63ba35a60f
Vitor Honorato <vittorhonoratoo@gmail.com>
2026-09-21 21:50:10 -0300

    configurando cors

--- src/main/java/com/vittorhonorato/caderno_dev/config/CorsConfig.java
+++ src/main/java/com/vittorhonorato/caderno_dev/config/CorsConfig.java
@@ -13,14 +13,25 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
-    private String[] allowedOrigins = {"http://localhost:4200"};
-    private String[] allowedMethods = {"GET", "POST", "PUT", "PATCH", "DELETE"};
+    private final String[] allowedOrigins = {
+            "http://localhost:4200",
+            "https://caderno-dev-frontend.onrender.com"
+    };
+
+    private final String[] allowedMethods = {
+            "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+    };
+
+    private final String[] allowedHeaders = {
+            "Authorization", "Content-Type"
+    };
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
         cors.setAllowedOrigins(Arrays.stream(allowedOrigins).toList());
         cors.setAllowedMethods(Arrays.stream(allowedMethods).toList());
+        cors.setAllowedHeaders(Arrays.stream(allowedHeaders).toList());
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cors);
